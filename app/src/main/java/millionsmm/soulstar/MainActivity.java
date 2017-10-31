2a5a5950d897c5da46ec63c87400e568bec3446b
millionsmm <linweipeng96@qq.com>
2017-10-31 17:36:34 +0800

    - create a show

--- app/src/main/java/millionsmm/soulstar/MainActivity.java
+++ app/src/main/java/millionsmm/soulstar/MainActivity.java
@@ -1,13 +1,20 @@
 package millionsmm.soulstar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SoulStarView soulStarView;
+    private TextStarAdapter textStarAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        soulStarView = (SoulStarView) findViewById(R.id.galaxy_view);
+        textStarAdapter = new TextStarAdapter(new String[20]);
+
+        soulStarView.setStarsAdapter(textStarAdapter);
     }
 }
